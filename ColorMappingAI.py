df9207cd76f2e9875bab88200c413d4763ea8b26
Lucie DN <ldellanegra@ensc.fr>
2023-11-14 21:05:20 +0100

    Modélisation du problème(graphe non orienté)

--- ColorMappingAI.py
+++ ColorMappingAI.py
@@ -1,14 +1,38 @@
+## Visualization
+
+# Create Random Graph
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Parameters
+nodes = ['A', 'B', 'C', 'D', 'E', 'F']
+edges = [('A', 'B'),('A', 'C'),('B', 'C'),('C', 'D'),('D', 'F'),('D', 'E'),('E', 'F')]
+
+
+# Create the corresponding graph
+G = nx.Graph()
+G.add_nodes_from(nodes)
+G.add_edges_from(edges)
+
+
+# Plot the graph
+plt.figure(figsize=(8,8))
+nx.draw(G, with_labels = True)
+plt.show()
+
+##
 
 zones = ["A", "B", "C", "D"]
 constraints = [("A", "B"), ("A", "C"), ("B", "C"), ("C", "D")]
 soluce = ['n', 'n', 'n', 'n']
 
 def Resolve(tab_soluce):
-    retour = True
+    bool = True
     for i in range(len(tab_soluce)):
         if tab_soluce[i] == 'n':
-            retour = False
-    return retour
+            bool = False
+    return bool
 
 
 
